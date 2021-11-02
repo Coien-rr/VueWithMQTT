fa8766adec5473cc743b349266a29c2259699f1e
Coienrr <83146518+Coien-rr@users.noreply.github.com>
2021-11-02 18:39:02 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+import ArcoVue from '@arco-design/web-vue';
+import App from './App.vue';
+import '@arco-design/web-vue/dist/arco.css';
+
+
+createApp(App)
+  .use(ArcoVue)
+  .mount('#app')
