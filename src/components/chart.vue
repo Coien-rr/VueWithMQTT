0d1c3575a8f1bdd223cda9db4f1eb9798645b865
Coienrr <83146518+Coien-rr@users.noreply.github.com>
2021-11-17 10:31:35 +0800

    Update dashborad

--- src/components/chart.vue
+++ src/components/chart.vue
@@ -74,6 +74,7 @@
       }
       store.subscribeSuccess = true;
       console.log('Subscribe to topics res', res);
+			Message.success('Subscribe to topics res!')
       });
   }
   
@@ -116,15 +117,15 @@
 <template>
   <main>
     <h1>Hello</h1>
-    <a-space>
+    <!-- <a-space>
       <a-button @click="call()" type="primary">Primary</a-button>
       <a-button @click="createConnection()" type="primary">connect</a-button>
       <a-button @click="doSubscribe()" type="primary">subscribe</a-button>
       <a-button @click="publish()" type="primary">Publish</a-button>
 			<a-alert type="success">This is an info alert.</a-alert>
-    </a-space>
+    </a-space> -->
 		
-			<div class="configuration" :style="{ display: 'flex' }">
+		<div class="configuration" :style="{ display: 'flex' }">
 				<a-card :style="{ width: '100%' }" title="Configuration">
 					<a-space direction="inline" size="large">
 						<a-form :model="form">
@@ -161,13 +162,74 @@
 								</a-col>
 							</a-row>
 							<a-row class="buttons">
-								<a-button @click="createConnection()" shape="round" type="primary" style="margin-right: 20px; " size="large">connect</a-button>
-								<a-button @click="destroyConnection()" shape="round" type="primary" status="danger" size="large">disconnect</a-button>
+								<a-button @click="createConnection()" shape="round" type="primary" style="margin-right: 20px; " size="large">Connect</a-button>
+								<a-button @click="destroyConnection()" shape="round" type="primary" status="danger" size="large">Disconnect</a-button>
 							</a-row>
 						</a-form>
 					</a-space>
 				</a-card>
   	</div>
+
+		<div class="ap">
+			<div class="subscribe" :style="{ display: 'flex' }">
+				<a-card :style="{ width: '600px' }" style="height: 240px;" title="Subscribe">
+					<a-form>
+						<a-cow>
+							<a-col :span = "22">
+								<a-form-item label="Topic">
+									<a-input v-model="store.subscription.topic" placeholder="please enter Topic..." />
+								</a-form-item>
+							</a-col>
+							<a-col :span = "22">
+								<a-form-item label="Qos">
+									<a-input v-model="store.subscription.qos" placeholder="please enter Qos..." />
+								</a-form-item>
+							</a-col>
+						</a-cow>
+						<a-row>
+							<a-col>
+								<a-row class="buttons">
+									<a-button @click="doSubscribe()" shape="round" type="primary" status = "success" style="margin-right: 20px; " size="large">Subscribe</a-button>
+									<a-button shape="round" type="primary" status="danger" size="large">unSubscribe</a-button>
+								</a-row>
+							</a-col>
+						</a-row>
+					</a-form>
+				</a-card>
+			</div>
+
+			<div class="publish" :style="{ display: 'flex'}">
+				<a-card :style="{ width: '600px' }" title="Publish">
+					<a-form>
+						<a-cow>
+							<a-col :span = "22">
+								<a-form-item label="Topic">
+									<a-input v-model="store.publish.topic" placeholder="please enter Topic..." />
+								</a-form-item>
+							</a-col>
+							<a-col :span = "22">
+								<a-form-item label="Payload">
+									<a-input v-model="store.publish.qos" placeholder="please enter Payload..." />
+								</a-form-item>
+							</a-col>
+							<a-col :span = "22">
+								<a-form-item label="QoS">
+									<a-input v-model="store.publish.payload" placeholder="please enter QoS..." />
+								</a-form-item>
+							</a-col>
+						</a-cow>
+						<a-row>
+							<a-col>
+								<a-row class="buttons">
+									<a-button @click="doPublish()" shape="round" type="primary" status = "success" style="margin-right: 20px; " size="large">Publish</a-button>
+								</a-row>
+							</a-col>
+						</a-row>
+					</a-form>
+				</a-card>
+			</div>
+		</div>
+		
   </main>  
 </template>
 
@@ -183,11 +245,37 @@
 		margin-top: 30px;
 	} 
 
+	main .subscribe,
+	main .publish {
+		margin-top: 30px;
+	}
+
 	main .buttons{
 		margin-top: 15px;
 		display: flex;
 		justify-content: flex-end;
 		margin-right: 20px;
+		margin-bottom: 10px;
+	}
+
+	main .subscribe .buttons {
+		display: flex;
+		flex-wrap: wrap;
+	}
+
+	main .subscribe .buttons,
+	main .publish .buttons{
+		margin-top: 5px;
+		margin-bottom: 5px;
+		justify-content: center;
+		/* margin-right: 40px; */
+	}
+
+	main .ap{
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		box-sizing: border-box;
 	}
 
 
